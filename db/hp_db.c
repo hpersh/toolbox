9398a9f5345588ba0d44983f1e491ea6e6f92fe1
Howard Persh <hpersh@hpersh-VirtualBox.(none)>
2014-01-16 15:28:23 -0800

    Sketch of db

---
+++ db/hp_db.c
@@ -0,0 +1,77 @@
+struct hp_db {
+  char     *dirname;
+  unsigned num_recs;
+};
+
+struct hp_db_iter {
+  struct hp_db *db;
+};
+
+int
+hp_db_open(struct hp_db *db, char *name)
+{
+}
+
+int
+hp_db_close(struct hp_db *db)
+{
+}
+
+int
+hp_db_insert(struct hp_db *db, char *key, unsigned data_len, char *data)
+{
+  chdir(db->dirname);
+
+  fd = creat(key);
+  
+  write(fd, data, data_len + 1);
+
+  close(fd);
+
+  return (0);
+}
+
+int
+hp_db_iter_first(struct hp_db *db, struct hp_db_iter *iter)
+{
+}
+
+int
+hp_db_iter_last(struct hp_db *db, struct hp_db_iter *iter)
+{
+}
+
+int
+hp_db_find(struct hp_db *db, struct hp_db_iter *iter, unsigned mode, unsigned key_len, char *key)
+{
+}
+
+int
+hp_db_iter_prev(struct hp_db_iter *iter)
+{
+}
+
+int
+hp_db_iter_next(struct hp_db_iter *iter)
+{
+}
+
+unsigned
+hp_db_iter_at_end(struct hp_db_iter *iter)
+{
+}
+
+int
+hp_db_iter_rec(struct hp_db_iter *iter, unsigned *key_len, char *key, unsigned *data_len, char *data)
+{
+}
+
+int
+hp_db_erase(struct hp_db_iter *iter)
+{
+}
+
+int
+hp_db_cnt(struct hp_db *db, unsigned *cnt)
+{
+}
