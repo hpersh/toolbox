ec42c986e59f6c3f15d370bf0920331d3b65b483
Howard Persh <hpersh@hpersh-VirtualBox.(none)>
2014-02-27 15:08:43 -0800

    Use better crc

--- ovm/ovm.c
+++ ovm/ovm.c
@@ -340,15 +340,13 @@ ovm_cl_dict(struct ovm *vm, unsigned type, unsigned r1)
 
 /***************************************************************************/
 
-#if 0
-
 static void
 crc_init(unsigned *r)
 {
   *r = ~0;
 }
 
-static unsigned crc32_tab[] = {
+static const unsigned crc32_tab[] = {
 	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
 	0xe963a535, 0x9e6495a3,	0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
 	0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
@@ -401,24 +399,6 @@ crc32(unsigned *r, unsigned n, unsigned char *p)
 
   return (*r);
 }
-
-#else
-
-static void
-crc_init(unsigned *r)
-{
-  *r = 0;
-}
-
-static unsigned
-crc32(unsigned *r, unsigned n, unsigned char *p)
-{
-  for ( ; n; --n, ++p)  *r = ((19 * *r) / 7) + *p;
-
-  return (*r);
-}
-
-#endif
 
 /***************************************************************************/
 
